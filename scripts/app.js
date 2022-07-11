063b1800ff3b5d28e88330691e6c87f830ad5b47
icarrington <isaiah.carrington@uconn.edu>
2022-07-11 17:10:00 -0400

    Initial Commit

---
+++ scripts/app.js
@@ -0,0 +1,120 @@
+const fruits = [
+  {name: 'Apple', src: "encoded-string-1.jpg"},{name: 'Banana', src: "encoded-string-2.jpg"},{name: 'Cherry', src: "encoded-string-3.jpg"},{name: 'Grape', src: "encoded-string-4.jpg"},{name: 'Orange', src: "encoded-string-5.jpg"},{name: 'Peach', src: "encoded-string-6.jpg"},{name: 'Pear', src: "encoded-string-7.jpg"},{name: 'Strawberry', src: "encoded-string-8.jpg"},{name: 'Watermelon', src: "encoded-string-9.jpg"}
+];
+
+let previousAnswers = new Set();
+let questionNumber = 1;
+let totalQuestions = 5;
+let selectedAnswer;
+let correctAnswer;
+let rightAnswers = 0;
+
+const img = document.getElementById('image');
+const answers = document.querySelectorAll('.answer');
+const answerBox = document.querySelector('.answers');
+const nextButton = document.getElementById('next');
+const questionContainer = document.querySelector('.question-container');
+const result = document.querySelector('.result');
+
+
+function random(min, max) {
+  return Math.floor(min + Math.random() * (max + 1 - min));
+}
+
+function populateAnswers(correct) {
+  let choices = [];
+  const correctIndex = random(0,3);
+  let wrongAnswers = fruits.filter(value => value.name != correct); //Array of wrong answers
+
+
+  while (choices.length < 4) {
+    let wrongFruit = wrongAnswers[random(0, wrongAnswers.length - 1)]; // get a random fruit from wrong answers to fill the choices
+    let i = choices.length; //contains current index of choices
+
+
+    if(i == correctIndex) {choices.push(correct)}
+    else if(!choices.includes(wrongFruit.name)) {choices.push(wrongFruit.name)}
+  }
+
+  for(let i = 0; i < answers.length; i++) {
+    answers[i].innerHTML = choices[i];
+  }
+}
+
+function generateQuestion() {
+
+  //get a random fruit to put into the image
+  let index = random(0, fruits.length - 1)
+  let fruit = fruits[index];
+  console.log(index, fruit);
+
+  if(previousAnswers.has(fruit.name)) {generateQuestion()}
+  else{
+    img.src = `images/${fruit.src}`;
+    previousAnswers.add(fruit.name);
+
+    correctAnswer = fruit.name;
+
+    populateAnswers(correctAnswer);
+  }
+
+}
+
+function goNext() {
+  
+  if(selectedAnswer == correctAnswer) {handleCorrect()}
+  else {handleIncorrect()}
+
+  
+  if(questionNumber == totalQuestions) {
+    questionContainer.innerHTML = `You got ${rightAnswers} / ${totalQuestions} answers right.<button id="again">Play Again </button>`;
+    again.onclick = function(event) {location.reload()};
+  };
+
+  setTimeout(() => {
+    result.classList.remove('correct');
+    result.classList.remove('incorrect');
+  }, 600);
+  questionNumber++;
+  number.innerHTML = `${questionNumber}/${totalQuestions}`;
+  generateQuestion();
+}
+
+function handleCorrect() {
+  result.classList.add('correct');
+  result.innerHTML = 'Correct';
+  rightAnswers++;
+}
+
+function handleIncorrect() {
+  result.classList.add('incorrect');
+  result.innerHTML = 'Incorrect';
+}
+
+generateQuestion();
+
+let number = document.getElementById('number');
+number.innerHTML = `${questionNumber}/${totalQuestions}`;
+
+let question = document.querySelector('.question');
+question.innerHTML = 'What fruit is this?';
+
+//Prevent people from dragging the image or saving it
+img.addEventListener('mousedown', (e) => e.preventDefault());
+img.addEventListener('contextmenu', (e) => e.preventDefault());
+
+answerBox.addEventListener('click', function(event) {
+  let target = event.target;
+  if(target.className != 'answer') return;
+
+  answers.forEach(answer => answer.classList.remove('selected'));
+
+  target.classList.add('selected');
+  
+  selectedAnswer = target.textContent;
+  
+});
+
+nextButton.addEventListener('click', (() => {
+  answers.forEach(answer => answer.classList.remove('selected'));
+  goNext()}));
